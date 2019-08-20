e0a4e4c5e06448086c561014b992f45f1bea449f
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-08-20 10:56:03 -0500

    Defining main methods for Data Access Object (DAO) for table professors

---
+++ app/src/main/java/com/gyros/roomdatabase/db/dao/ProfessorDao.java
@@ -0,0 +1,37 @@
+package com.gyros.roomdatabase.db.dao;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.gyros.roomdatabase.constants.Constants;
+import com.gyros.roomdatabase.db.entity.Professor;
+
+import java.util.List;
+
+@Dao
+public interface ProfessorDao {
+
+    @Insert
+    void insertProfessor(Professor professor);
+
+    @Query("SELECT * FROM "+ Constants.NAME_TABLE_PROFESSOR)
+    List<Professor> getAllProfessors();
+
+    @Query("SELECT * FROM "+Constants.NAME_TABLE_PROFESSOR+" WHERE name LIKE :name")
+    Professor findProfessorByName(String name);
+
+    @Query("SELECT * FROM "+Constants.NAME_TABLE_PROFESSOR+" WHERE id = :id")
+    Professor findProfessorById(int id);
+
+    @Update
+    void updateProfessorById(Professor professor);
+
+    @Query("DELETE FROM "+Constants.NAME_TABLE_PROFESSOR)
+    void deleteAllProfessors();
+
+    @Delete
+    void deleteProfessorById(Professor professor);
+}
