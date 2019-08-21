418ec1e668f2a784319295e4f68901787b69ece3
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-08-21 10:43:21 -0500

    Create ProfessorLanguageDao, Insert a new register, get all languages by professor, and get all de professors by language.

---
+++ app/src/main/java/com/gyros/roomdatabase/db/dao/ProfessorLanguageDao.java
@@ -0,0 +1,31 @@
+package com.gyros.roomdatabase.db.dao;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.gyros.roomdatabase.constants.Constants;
+import com.gyros.roomdatabase.db.entity.Language;
+import com.gyros.roomdatabase.db.entity.Professor;
+import com.gyros.roomdatabase.db.entity.ProfessorLanguage;
+
+import java.util.List;
+
+@Dao
+public interface ProfessorLanguageDao {
+
+    @Insert
+    void insert(ProfessorLanguage professorLanguage);
+
+    @Query("SELECT * FROM "+ Constants.NAME_TABLE_PROFESSOR+
+            " INNER JOIN "+Constants.NAME_TABLE_PROFESSORS_LANGUAGES
+            +" ON "+Constants.NAME_TABLE_PROFESSOR+".id = "+Constants.NAME_TABLE_PROFESSORS_LANGUAGES+".professor_id WHERE "
+            +Constants.NAME_TABLE_PROFESSORS_LANGUAGES+".professor_id = :idLanguage")
+    List<Professor> getProfessorsByLanguage(int idLanguage);
+
+    @Query("SELECT * FROM "+ Constants.NAME_TABLE_LANGUAGES+
+            " INNER JOIN "+Constants.NAME_TABLE_PROFESSORS_LANGUAGES
+            +" ON "+Constants.NAME_TABLE_LANGUAGES+".id = "+Constants.NAME_TABLE_PROFESSORS_LANGUAGES+".language_id WHERE "
+            +Constants.NAME_TABLE_PROFESSORS_LANGUAGES+".language_id = :idProfessor")
+    List<Language> getLanguagesByProfessor(int idProfessor);
+}
